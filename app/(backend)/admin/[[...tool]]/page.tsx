f168ac463c596d4f144059821de076429321973a
accweb-20 <web@acc.gov.af>
2026-04-06 13:09:57 +0430

    Update page.tsx

--- app/(backend)/admin/[[...tool]]/page.tsx
+++ app/(backend)/admin/[[...tool]]/page.tsx
@@ -1,17 +1,20 @@
-// next.config.ts
-import type { NextConfig } from "next";
+// app/(backend)/[[...tool]]/page.tsx
+/**
+ * This route is responsible for the built-in authoring environment using Sanity Studio.
+ * All routes under your studio path is handled by this file using Next.js' catch-all routes:
+ * https://nextjs.org/docs/routing/dynamic-routes#catch-all-routes
+ *
+ * You can learn more about the next-sanity package here:
+ * https://github.com/sanity-io/next-sanity
+ */
 
-const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "cdn.sanity.io",
-        port: "",
-        pathname: "/images/**",
-      },
-    ],
-  },
-};
+import { NextStudio } from 'next-sanity/studio'
+import config from '../../../../sanity.config'
 
-export default nextConfig;
+export const dynamic = 'force-static'
+
+export { metadata, viewport } from 'next-sanity/studio'
+
+export default function StudioPage() {
+  return <NextStudio config={config} />
+}
