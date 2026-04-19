a939cc8ec2671eb13745b43971e26f36435264b3
accweb-20 <web@acc.gov.af>
2026-04-19 15:33:03 +0430

    Update SalesGrid.tsx

--- components/Sales/SalesGrid.tsx
+++ components/Sales/SalesGrid.tsx
@@ -53,7 +53,7 @@ export default function SalesGrid({
   return (
     <section id="sales" className="w-full mx-auto mt-10 text-[#1A1A1A] bg-white">
       <div className="flex justify-center">
-        <div className="mx-auto w-[90%] md:w-[93%] lg:w-[90%] max-w-[493px] md:max-w-[924px] lg:max-w-[1440px]">
+        <div className="mx-auto w-[90%] md:w-[93%] lg:w-[90%] max-w-[493px] md:max-w-[924px] lg:max-w-[1140px] py-7 md:py-8">
           <h2 className="text-[45px] md:text-[60px] leading-none font-extrabold tracking-wide text-[#1A1A1A] mb-6">
             SALES ITEMS
           </h2>
